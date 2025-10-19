9d9a1ac8bdbf5137a9b1d47286c1b63eb3c3deaa
nerikeb <nerike.b@gmail.com>
2025-10-19 19:40:29 +0200

    Added two methods for subtraction and addition with doubles

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    static double Addition(double a, double b) {
+        return a + b;
+    }
+
+    static double Subtraction(double a, double b) {
+        return a - b;
+    }
+}
